54e46b5aeea0a8882abb26bc63dbf6c73682b6eb
testinspiration <testinspiration@live.com>
2013-01-04 17:49:19 +0100

    fix a bug

--- EventBooking.Tests/Controllers/HomeControllerTest.cs
+++ EventBooking.Tests/Controllers/HomeControllerTest.cs
@@ -22,7 +22,7 @@ namespace EventBooking.Tests.Controllers
             ViewResult result = controller.Index() as ViewResult;
 
             // Assert
-            Assert.AreEqual("Modify this template to jump-start your ASP.NET MVC application.", result.ViewBag.Message);
+            Assert.AreEqual("Ride On", result.ViewBag.Message);
         }
 
         [TestMethod]
