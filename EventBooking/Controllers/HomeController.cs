12c50b1989c0c699a5ba513fbebcf0940367546e
testinspiration <testinspiration@live.com>
2013-01-04 17:45:26 +0100

    testing the push to appharbour

--- EventBooking/Controllers/HomeController.cs
+++ EventBooking/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace EventBooking.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+            ViewBag.Message = "Ride On";
 
             return View();
         }
