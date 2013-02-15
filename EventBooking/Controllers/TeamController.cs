426247d093d531cf5e23f87623c20360176c4ffc
Peo <peo@jayway.com>
2013-02-15 19:59:52 +0100

    Added a Team details page

---
+++ EventBooking/Controllers/TeamController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Web.Mvc;
+using EventBooking.Data;
+using EventBooking.Extensions;
+
+namespace EventBooking.Controllers
+{
+    public class TeamController : Controller
+    {
+        public ActionResult Details(int id)
+        {
+            return View(new Team
+                {
+                    Activities = new[]
+                         {
+                             new Activity
+                                 {
+                                     Name = "Fake activity",
+                                     Description = "A description",
+                                     Date = DateTime.Now,
+                                     Summary = "A summary",
+                                     Coordinator = new User {Name = "Tomten"},
+                                     RequiredItems = new Item[]{},
+                                     Sessions = new Session[0],
+                                     Type = ActivityType.Public
+                                 },
+                                 new Activity
+                                 {
+                                     Name = "Fake activity2",
+                                     Description = "A description",
+                                     Date = 30.Days().FromNow(),
+                                     Summary = "A summary",
+                                     Coordinator = new User {Name = "Tomten"},
+                                     RequiredItems = new Item[]{},
+                                     Sessions = new Session[0],
+                                     Type = ActivityType.Public
+                                 },
+                         },
+                    Name = "Malmö"
+                });
+        }
+    }
+}
---
+++ EventBooking/Extensions/DateExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EventBooking.Extensions
+{
+    public static class TimeSpanExtensions
+    {
+         public static DateTime FromNow(this TimeSpan span)
+         {
+             return DateTime.Now + span;
+         }
+    }
+}
