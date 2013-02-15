5bbc1d802d3d3060a6809636713e38aa1d8d3a1d
Sami Lamti <sami.lamti@gmail.com>
2013-02-15 19:42:56 +0100

    Added Test project and SpecFlow

---
+++ EventBooking.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EventBooking.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
