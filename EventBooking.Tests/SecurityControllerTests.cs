1148e0943f857f81803649f7d0a0dea79bb90577
Sami Lamti <sami.lamti@gmail.com>
2013-02-15 20:02:56 +0100

    Added LoginModel

---
+++ EventBooking.Tests/SecurityControllerTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Web.Mvc;
+using EventBooking.Controllers.ViewModels;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EventBooking.Tests
+{
+    [TestClass]
+    public class SecurityControllerTests
+    {
+        [TestMethod]
+        public void Index_Returns_View()
+        {
+            var controller = new SecurityController();
+
+            var result = controller.Index();
+
+            var viewResult = result as ViewResult;
+            Assert.IsNotNull(viewResult);
+            Assert.IsInstanceOfType(viewResult.Model, typeof(LoginModel));
+
+        }
+    }
+}
---
+++ EventBooking/Controllers/ViewModels/LoginModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Security;
+
+namespace EventBooking.Controllers.ViewModels
+{
+    public class LoginModel
+    {
+        public string ElectronicMailAddress { get; set; }
+        public SecureString Password { get; set; }
+    }
+}
