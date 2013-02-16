bc9d41f436ce20818b0c85aae41c7f9e5dacde3a
Johan Andersson <johan-spam@andersson.net>
2013-02-16 15:39:57 +0100

    Breaking up classes to separate files

---
+++ EventBooking/Services/SecurityService.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using EventBooking.Data;
+using WebMatrix.WebData;
+
+namespace EventBooking.Services
+{
+	public class SecurityService : ISecurityService
+	{
+		public virtual User GetUser(string userName)
+		{
+			int userId = WebSecurity.GetUserId(userName);
+			using (var context = new EventBookingContext())
+			{
+				return context.Users.Find(userId);
+			}
+		}
+
+		public virtual bool SignIn(string userName, string password)
+		{
+			return WebSecurity.Login(userName, password);
+		}
+
+		public void CreateUserAndAccount(string email, string password, DateTime created)
+		{
+			DateTime earlier = DateTime.UtcNow;
+
+			WebSecurity.CreateUserAndAccount(email, password, new {Created = earlier});
+
+			using (var context = new EventBookingContext())
+			{
+				context.Users.Add(new User
+					{
+						Email = email,
+						Id = WebSecurity.GetUserId(email),
+						Created = earlier
+					});
+				context.SaveChanges();
+			}
+		}
+
+		public void SignOff()
+		{
+			WebSecurity.Logout();
+		}
+
+		public virtual User CurrentUser
+		{
+			get { return GetUser(WebSecurity.CurrentUserName); }
+		}
+
+		public virtual bool IsLoggedIn
+		{
+			get { return WebSecurity.IsAuthenticated; }
+		}
+	}
+}
