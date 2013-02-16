c4d59426882cf946feb5b0c4374c7f268c1a5992
Martin Mazur <m.m@upplopp.se>
2013-02-16 18:04:40 +0100

    Added js for forward backward stuff between activities and things.

--- EventBooking/Controllers/ViewModels/UpcomingActivitiesModel.cs
+++ EventBooking/Controllers/ViewModels/UpcomingActivitiesModel.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using System.Web.Mvc;
 using EventBooking.Data;
 
 namespace EventBooking.Controllers.ViewModels
@@ -7,16 +8,10 @@ namespace EventBooking.Controllers.ViewModels
     public class UpcomingActivitiesModel
     {
         public IEnumerable<ActivityModel> Activities { get; set; }
-
-        public string NextUrl { get; set; }
-
-        public string PreviousUrl { get; set; }
         
         public UpcomingActivitiesModel(IEnumerable<Activity> activityData)
         {
             this.Activities = activityData.Select(data => new ActivityModel(data));
-            this.NextUrl = "http://www.example.com";
-            this.PreviousUrl = "http://www.example.com";
         }
     }
 }
