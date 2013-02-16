3a9f744b873e00d7e75d0d29cf9be315ecfb7157
Johan Andersson <johan-spam@andersson.net>
2013-02-16 15:42:18 +0100

    Added Add method to repo

--- EventBooking.Data/Repositories/ActivityRepository.cs
+++ EventBooking.Data/Repositories/ActivityRepository.cs
@@ -1,31 +1,34 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Data.Entity;
+using System.Linq;
 
 namespace EventBooking.Data.Repositories
 {
-    public sealed class ActivityRepository : IActivityRepository
-    {
-        private readonly IEventBookingContext _context;
+	public class ActivityRepository : IActivityRepository
+	{
+		private readonly IEventBookingContext _context;
 
-        public ActivityRepository(IEventBookingContext context)
-        {
-            _context = context;
-        }
+		public ActivityRepository(IEventBookingContext context)
+		{
+			_context = context;
+		}
 
-        public IQueryable<Activity> GetActivityByMonth(int year, int month, int teamId = 0)
-        {
-            var query = _context.Activities.Include(x => x.OrganizingTeam)
-                .Where(x => x.Date.Year == year && x.Date.Month == month);
-            if (teamId > 0)
-            {
-                // Filter on team as well.
-                query = query.Where(x => x.OrganizingTeam.Id == teamId);
-            }
-            return query;
-        }
-    }
+		public IQueryable<Activity> GetActivityByMonth(int year, int month, int teamId = 0)
+		{
+			IQueryable<Activity> query = _context.Activities.Include(x => x.OrganizingTeam)
+			                                     .Where(x => x.Date.Year == year && x.Date.Month == month);
+			if (teamId > 0)
+			{
+				// Filter on team as well.
+				query = query.Where(x => x.OrganizingTeam.Id == teamId);
+			}
+			return query;
+		}
+
+		public virtual void Add(Activity activity)
+		{
+			_context.Activities.Add(activity);
+			_context.SaveChanges();
+		}
+	}
 }
--- EventBooking.Data/Repositories/IActivityRepository.cs
+++ EventBooking.Data/Repositories/IActivityRepository.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Linq;
 
 namespace EventBooking.Data.Repositories
 {
-    public interface IActivityRepository
-    {
-        IQueryable<Activity> GetActivityByMonth(int year, int month, int teamId = 0);
-    }
+	public interface IActivityRepository
+	{
+		IQueryable<Activity> GetActivityByMonth(int year, int month, int teamId = 0);
+	}
 }
