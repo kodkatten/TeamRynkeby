019d9e9de27f655c98c32df7fc714fd60afc72a1
Erik Forsberg <erik.forsberg@tretton37.com>
2013-02-16 21:23:15 +0100

    Added some team selector on landing page

--- EventBooking/Controllers/HomeController.cs
+++ EventBooking/Controllers/HomeController.cs
@@ -9,13 +9,13 @@ namespace EventBooking.Controllers
     public class HomeController : Controller
     {
         private readonly ISecurityService _securityService;
-        private readonly ITeamRepository _team;
+        private readonly ITeamRepository _teamRepository;
         private readonly ActivityRepository _activityRepository;
 
-        public HomeController(ISecurityService security, ITeamRepository team, ActivityRepository activityRepository)
+        public HomeController(ISecurityService security, ITeamRepository teamRepository, ActivityRepository activityRepository)
         {
             _securityService = security;
-            _team = team;
+            _teamRepository = teamRepository;
             _activityRepository = activityRepository;
         }
 
@@ -23,7 +23,6 @@ namespace EventBooking.Controllers
         {
             bool isNobody = true;
 
-            // Got an authenticated user?
             if (_securityService.IsLoggedIn)
             {
                 var user =  _securityService.CurrentUser;
@@ -33,13 +32,9 @@ namespace EventBooking.Controllers
                 }
             }
 
-            // Get all teams.
-            ViewBag.Teams = _team.GetTeams().ToArray();
-
             var nextTwoActivities = _activityRepository.GetUpcomingActivities(0, 2);
 
-            // Create the model.
-            var model = new LandingPageModel(nextTwoActivities) { IsNobody = isNobody };
+            var model = new LandingPageModel(nextTwoActivities,_teamRepository.GetTeams()) { IsNobody = isNobody };
             return View(model);
         }
     }
