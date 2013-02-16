910e58de2769eca43dd4cd3c4284dc0a8b7315b8
Johan Andersson <johan-spam@andersson.net>
2013-02-16 15:43:35 +0100

    Moved extension method

--- EventBooking/Controllers/ViewModels/CreateActivityModel.cs
+++ EventBooking/Controllers/ViewModels/CreateActivityModel.cs
@@ -1,10 +1,5 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
-using System.Linq.Expressions;
-using System.Text;
-using System.Web;
-using System.Web.Mvc;
-using System.Web.Mvc.Html;
 using EventBooking.Data;
 
 namespace EventBooking.Controllers.ViewModels
@@ -30,36 +25,5 @@ namespace EventBooking.Controllers.ViewModels
 
 		[Display( Name = "Mer information" )]
 		public string Summary { get; set; }
-	}
-
-	public static class HtmlExtensions
-	{
-		public static MvcHtmlString RadioButtonForEnum<TModel, TProperty>(
-			this HtmlHelper<TModel> htmlHelper,
-			Expression<Func<TModel, TProperty>> expression
-		)
-		{
-			var metaData = ModelMetadata.FromLambdaExpression( expression, htmlHelper.ViewData );
-			var names = Enum.GetNames( metaData.ModelType );
-			var sb = new StringBuilder();
-			foreach ( var name in names )
-			{
-				var id = string.Format(
-					"{0}_{1}_{2}",
-					htmlHelper.ViewData.TemplateInfo.HtmlFieldPrefix,
-					metaData.PropertyName,
-					name
-				);
-
-				var radio = htmlHelper.RadioButtonFor( expression, name, new { id = id } ).ToHtmlString();
-				sb.AppendFormat(
-					"{0} <label for=\"{1}\">{2}</label>",
-					radio,
-					id,
-					HttpUtility.HtmlEncode( name )
-				);
-			}
-			return MvcHtmlString.Create( sb.ToString() );
-		}
 	}
 }
