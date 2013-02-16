e83e50a302f9e5743cc44a1680bc2e3912e5f61c
Sami Lamti <sami.lamti@gmail.com>
2013-02-16 17:20:55 +0100

    Broke login, sorry, fixing now

--- EventBooking/Services/ISecurityService.cs
+++ EventBooking/Services/ISecurityService.cs
@@ -12,5 +12,6 @@ namespace EventBooking.Services
 		void SignOff();
 		bool SignIn(string username, string password);
 		void CreateUserAndAccount(string email, string password, DateTime created);
+	    User GetUser(string electronicMailAddress);
 	}
 }
