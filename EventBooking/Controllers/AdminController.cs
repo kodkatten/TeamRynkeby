3d8742109c51ece44d0ddd3c8694d3197ada77ab
Andreas <andreas.nilsson@tretton37.com>
2013-02-16 14:30:54 +0100

    Added administration page

---
+++ EventBooking/Controllers/AdminController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EventBooking.Controllers.ViewModels;
+using EventBooking.Data;
+
+namespace EventBooking.Controllers
+{
+    public class AdminController : Controller
+    {
+        //
+        // GET: /Admin/
+
+        public ActionResult Index()
+        {
+            AdministratorPageModel model = new AdministratorPageModel(new List<Team>());
+            return View(model);
+        }
+
+    }
+}
---
+++ EventBooking/Views/Admin/AdministratorPageModel.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using EventBooking.Data;
+
+namespace EventBooking.Controllers.ViewModels
+{
+    public class AdministratorPageModel
+    {
+        public AdministratorPageModel(IEnumerable<Team> teams)
+        {
+            this.Teams = teams;
+        }
+
+        public IEnumerable<Team> Teams { get; private set; }
+
+
+    }
+}
