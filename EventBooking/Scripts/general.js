f173a8a85df24bc89c016ca9ba7fd30822d9ec5b
Martin Mazur <m.m@upplopp.se>
2013-02-17 17:43:50 +0100

    Added automatic redirect after 3 seconds.

--- EventBooking/Scripts/general.js
+++ EventBooking/Scripts/general.js
@@ -1,15 +1,30 @@
 ﻿
-$(document).ready(function () {
-    $('[rel=tooltip]').tooltip({
-        placement: "right"
-    });
-});
+(function($, undefined) {
+    var initRedirect = function () {
+        var redirectUrl = $('a.redirect:first').attr("href");
 
-$(function () {
-    $('.datepicker').pickadate();
-    $('.timepicker').timepicker({
-        minuteStep: 15,
-        showSeconds: false,
-        showMeridian: false
+        if (redirectUrl !== undefined) {
+            setTimeout(function () {
+                window.location.href = redirectUrl;
+            }, 3000);
+        }
+    };
+
+    $(document).ready(function () {
+        $('[rel=tooltip]').tooltip({
+            placement: "right"
+        });
+
+        initRedirect();
     });
-});
+
+    $(function () {
+        $('.datepicker').pickadate();
+        $('.timepicker').timepicker({
+            minuteStep: 15,
+            showSeconds: false,
+            showMeridian: false
+        });
+    });
+}(jQuery));
+
