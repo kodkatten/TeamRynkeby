4f1a983b6de33483a5ff43e1956d09253e12232b
Johan Andersson <johan-spam@andersson.net>
2013-02-17 12:56:59 +0100

    Needs route Sessions/Index/activityId to work!

--- EventBooking/App_Start/RouteConfig.cs
+++ EventBooking/App_Start/RouteConfig.cs
@@ -12,15 +12,17 @@ namespace EventBooking
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
-            routes.MapRoute(
+			/* todo 
+	        routes.MapRoute(
+		        name: "SessionsForActivity",
+		        url: "Sessions/Index/{activityId}"
+		        );
+			 */
+	        routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
-
-  
-
         }
     }
 }
