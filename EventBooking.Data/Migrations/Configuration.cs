e262e5589b05da0d151026bb7c1ab4385f667d4a
testinspiration <testinspiration@live.com>
2013-03-07 23:00:37 +0100

    ....

--- EventBooking.Data/Migrations/Configuration.cs
+++ EventBooking.Data/Migrations/Configuration.cs
@@ -28,39 +28,6 @@ namespace EventBooking.Data.Migrations
 			    context.SaveChanges();
 				
 			}
-            if (!context.Users.Any())
-            {
-                var user = new User
-                    {
-                        Name = "Henrik Andersson",
-                        Cellphone = "123456",
-                        Created = DateTime.UtcNow                        
-                    };
-                context.Users.Add(user);
-
-            }
-            if (!context.Activities.Any())
-            {
-                //var activty = new Activity
-                //    {
-                //        Date = DateTime.Now,
-                //        Name = "testactivity",
-                //        Description = "test",
-                //        Summary = "Test"
-                //    };
-                //var ts = context.Teams.Where(t => t.Name == "Team Stockholm");
-
-                //foreach (var team in ts)
-                //{
-                //    activty.OrganizingTeam = team;
-                //}
-                //var session = new Session {ToTime = TimeSpan.MaxValue, FromTime = TimeSpan.MinValue};
-                //session.Activity = activty;
-
-                //activty.Sessions.Add(session);
-                
-            }
-            
 		}
 	}
 }
