9d7d19200b818abcea5628654baf5cfb167734de
Niklas Dahlman <initials@nida.se>
2013-04-02 11:57:59 +0200

    add custom items

--- EventBooking/Scripts/teamrynkebyse.itemsbuilder.js
+++ EventBooking/Scripts/teamrynkebyse.itemsbuilder.js
@@ -3,9 +3,11 @@ var teamrynkebyse = teamrynkebyse === undefined ? {} : teamrynkebyse;
 teamrynkebyse.itemsbuilder = (function () {
 
     var addMoreItemsId = '#addMoreItems';
+    var addSpecificItemsId = '#addSpecificItem';
     var itemsContainerId = '#itemsContainer';
     var undoClass = '.undo';
     var itemsTemplateId = 'itemsTemplate';
+    var specificItemsTemplateId = 'specificItemsTemplate';
     var count = 0;
     
     function addItem() {
@@ -22,9 +24,24 @@ teamrynkebyse.itemsbuilder = (function () {
         });
     }
 
+    function addSpecificItem() {
+        count++;
+        var viewData = { index: count };
+        var result = $(itemsContainerId)
+                        .mustache(specificItemsTemplateId, viewData)
+                        .children()
+                        .last();
+
+        result.find(undoClass).on('click', function () {
+            result.remove();
+            count--;
+        });
+    }
+
     return {
         init: function () {
             $(addMoreItemsId).on('click', addItem);
+            $(addSpecificItemsId).on('click', addSpecificItem);
         }
     };
 })();
